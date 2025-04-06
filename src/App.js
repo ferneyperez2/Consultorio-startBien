b954c05892be7d1739a893b68601716bd82baa5c
ferney perez <perezdehoyosferney@gmail.com>
2025-04-06 12:44:29 -0500

    Guardando cambios locales antes del pull

--- src/App.js
+++ src/App.js
@@ -1,68 +1,4 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./Component/Home";
-// import Services from "./Component/Servicio";
-// import Contact from "./Component/Contact";
-// import AgendarCita from "./Component/BookingSection";
-// import Sidebar from "./Component/Sidebar";
-// import Header from "./Component/Header";
-// import Testimonios from "./Component/Testimonios";
-// import React, { useState, useEffect } from "react";
-// import { useTheme } from '../src/Component/ThemeContext'; // Asegúrate de que la ruta sea correcta
-
-// import './App.css';
-// import Navbar from "./Component/Navbar";
-// function App() {
-
-//   const { isDarkMode } = useTheme();
-
-//   useEffect(() => {
-//     // 
-//     if (isDarkMode) {
-//       document.body.classList.add('dark-mode');
-//     } else {
-//       document.body.classList.remove('dark-mode');
-//     }
-//   }, [isDarkMode]);
-
-//   const [modoOscuro, setModoOscuro] = useState(
-//     localStorage.getItem("theme") === "dark"
-//   );
-
-//   useEffect(() => {
-//     if (modoOscuro) {
-//       document.documentElement.classList.add("dark-mode");
-//     } else {
-//       document.documentElement.classList.remove("dark-mode");
-//     }
-//     localStorage.setItem("theme", modoOscuro ? "dark" : "light");
-//   }, [modoOscuro]);
-//   return (
-//     <Router>
-//        <div className={modoOscuro ? "dark-mode" : ""}>
-//       <Header modoOscuro={modoOscuro} setModoOscuro={setModoOscuro} />
-     
-//     </div>
-
-//       <Header/>
-//       <Sidebar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/services" element={<Services />} />
-//         <Route path="/contact" element={<Contact />} />
-//         {/* <Route path="/testimonio" element={<Testimonios />} /> */}
-//         <Route path="/agendar-cita" element={<AgendarCita />} />
-//       </Routes>
-
-  
-
-//     </Router>
-
-    
-//   );
-// }
-
-// export default App;
-
+// Consultorio-startBien/src/App.js
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Component/Home";
 import Services from "./Component/Servicio";
@@ -70,12 +6,8 @@ import Contact from "./Component/Contact";
 import AgendarCita from "./Component/BookingSection";
 import Sidebar from "./Component/Sidebar";
 import Header from "./Component/Header";
-import Testimonios from "./Component/Testimonios";
 import React, { useState, useEffect } from "react";
-// import AOS from "aos";
-// import "aos/dist/aos.css";
 
-// AOS.init();
 
 import './App.css';
 
